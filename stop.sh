6440b82a51c4272e06b9b44cb33924a923e2c03e
Jonathan Elchison <JElchison@Gmail.com>
2017-12-19 08:42:48 -0500

    Update stop.sh

--- stop.sh
+++ stop.sh
@@ -3,6 +3,7 @@
 # don't use -e, since we want to stop as many things as possible
 set -ufx
 
-sudo logkeys --kill
+pkill node
 sudo pkill -f 'sudo.*bgrep'
 sudo pkill -f logkeys.log
+sudo logkeys --kill
