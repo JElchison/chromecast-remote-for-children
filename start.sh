164ed25069d9b791c516cf1f11b2e2114f90219a
Jonathan Elchison <JElchison@Gmail.com>
2017-12-19 08:38:13 -0500

    Add other read priv on keylog file; Remove sudo; Re-add Bash functions

--- start.sh
+++ start.sh
@@ -11,7 +11,22 @@ CHROMECAST_DEV=$(jq -r '.chromecast_dev' $CONFIG_JSON)
 VOLUME_STEP=0.25
 
 
+killall_castnow_processes() {
+    pkill node
+}
+
+play_youtube() {
+    youtube-dl -o - "$1" | castnow --device "$CHROMECAST_DEV" --quiet -
+}
+
+play_youtube_playlist_random() {
+    youtube-dl --playlist-random -o - "$1" | castnow --device "$CHROMECAST_DEV" --quiet -
+}
+
+
 # start keylogger
+touch $LOG_FILE
+chmod o+r $LOG_FILE
 sudo logkeys --start --device="$INPUT_DEV"
 
 # when this script is killed, also kill all sub-jobs
@@ -21,136 +36,136 @@ trap "trap - SIGTERM && kill -- -$$" SIGINT SIGTERM EXIT
 # control functions
 #
 
-sudo bash -c "while :; do
+while :; do
     tail -f -n0 $LOG_FILE | stdbuf -o0 bgrep -b \\\"\<Esc\>\\\" | while read -r; do
         echo ===== Stop =====
-        pkill node
+        killall_castnow_processes
         castnow --device $CHROMECAST_DEV --command s --exit
     done
-done" &
+done &
 
-sudo bash -c "while :; do
+while :; do
     tail -f -n0 $LOG_FILE | stdbuf -o0 bgrep -b \\\"\<Tab\>\\\" | while read -r; do
         echo ===== Next =====
         castnow --device $CHROMECAST_DEV --command n --exit
     done
-done" &
+done &
 
-sudo bash -c "while :; do
+while :; do
     tail -f -n0 $LOG_FILE | stdbuf -o0 bgrep -b \\\"\<KP+\>\\\" | while read -r; do
         echo ===== Volume Up =====
         castnow --device $CHROMECAST_DEV --command up --volume-step $VOLUME_STEP --exit
     done
-done" &
+done &
 
-sudo bash -c "while :; do
+while :; do
     tail -f -n0 $LOG_FILE | stdbuf -o0 bgrep -b \\\"\<KP-\>\\\" | while read -r; do
         echo ===== Volume Down =====
         castnow --device $CHROMECAST_DEV --command down --volume-step $VOLUME_STEP --exit
     done
-done" &
+done &
 
 #
 # numpad mappings
 #
 
-sudo bash -c "while :; do
+while :; do
     tail -f -n0 $LOG_FILE | stdbuf -o0 bgrep -b \\\"\<KP0\>\\\" | while read -r; do
         echo ===== Playing Polaroid Memories =====
-        pkill node
-        youtube-dl -o - https://www.youtube.com/watch?v=shNP5H13a1M | castnow --device "$CHROMECAST_DEV" --quiet -
+        killall_castnow_processes
+        play_youtube https://www.youtube.com/watch?v=shNP5H13a1M
     done
-done" &
+done &
 
-sudo bash -c "while :; do
+while :; do
     tail -f -n0 $LOG_FILE | stdbuf -o0 bgrep -b \\\"\<KP1\>\\\" | while read -r; do
         echo ===== Playing Claymation Christmas =====
-        pkill node
-        youtube-dl -o - https://www.youtube.com/playlist?list=PLd6uqVNEu-peflL0eT4lOwVrRnshaod98 | castnow --device "$CHROMECAST_DEV" --quiet -
+        killall_castnow_processes
+        play_youtube https://www.youtube.com/playlist?list=PLd6uqVNEu-peflL0eT4lOwVrRnshaod98
     done
-done" &
+done &
 
-sudo bash -c "while :; do
+while :; do
     tail -f -n0 $LOG_FILE | stdbuf -o0 bgrep -b \\\"\<KP2\>\\\" | while read -r; do
         echo ===== Playing The Snowman =====
-        pkill node
-        youtube-dl -o - https://www.youtube.com/watch?v=ZE9KpobX9J8 | castnow --device "$CHROMECAST_DEV" --quiet -
+        killall_castnow_processes
+        play_youtube https://www.youtube.com/watch?v=ZE9KpobX9J8
     done
-done" &
+done &
 
-sudo bash -c "while :; do
+while :; do
     tail -f -n0 $LOG_FILE | stdbuf -o0 bgrep -b \\\"\<KP3\>\\\" | while read -r; do
         echo ===== Playing Carl the Super Truck =====
-        pkill node
-        youtube-dl -o - https://www.youtube.com/watch?v=JUeru7ioMyk | castnow --device "$CHROMECAST_DEV" --quiet -
+        killall_castnow_processes
+        play_youtube https://www.youtube.com/watch?v=JUeru7ioMyk
     done
-done" &
+done &
 
-sudo bash -c "while :; do
+while :; do
     tail -f -n0 $LOG_FILE | stdbuf -o0 bgrep -b \\\"\<KP4\>\\\" | while read -r; do
         echo ===== Playing Mister Rogers =====
-        pkill node
-        youtube-dl --playlist-random -o - https://www.youtube.com/playlist?list=PLf22vOV7unX-ly5LC98UyZTOSG8ckDqmt | castnow --device "$CHROMECAST_DEV" --quiet -
+        killall_castnow_processes
+        play_youtube_playlist_random https://www.youtube.com/playlist?list=PLf22vOV7unX-ly5LC98UyZTOSG8ckDqmt
     done
-done" &
+done &
 
-sudo bash -c "while :; do
+while :; do
     tail -f -n0 $LOG_FILE | stdbuf -o0 bgrep -b \\\"\<KP5\>\\\" | while read -r; do
         echo ===== Playing Daniel Tiger =====
-        pkill node
-        youtube-dl --playlist-random -o - https://www.youtube.com/playlist?list=PLKzJHcOwWn3KWJco0w6gVvhcrhXDb4TFi | castnow --device "$CHROMECAST_DEV" --quiet -
+        killall_castnow_processes
+        play_youtube_playlist_random https://www.youtube.com/playlist?list=PLKzJHcOwWn3KWJco0w6gVvhcrhXDb4TFi
     done
-done" &
+done &
 
-sudo bash -c "while :; do
+while :; do
     tail -f -n0 $LOG_FILE | stdbuf -o0 bgrep -b \\\"\<KP6\>\\\" | while read -r; do
         echo ===== Playing Thomas and Friends =====
-        pkill node
-        youtube-dl --playlist-random -o - https://www.youtube.com/playlist?list=PLI2i4PrLia3gAAYjMaVbAwdt1xrOsUaXS | castnow --device "$CHROMECAST_DEV" --quiet -
+        killall_castnow_processes
+        play_youtube_playlist_random https://www.youtube.com/playlist?list=PLI2i4PrLia3gAAYjMaVbAwdt1xrOsUaXS
     done
-done" &
+done &
 
-sudo bash -c "while :; do
+while :; do
     tail -f -n0 $LOG_FILE | stdbuf -o0 bgrep -b \\\"\<KP7\>\\\" | while read -r; do
         echo ===== Playing Colored Lullaby =====
-        pkill node
-        youtube-dl -o - https://www.youtube.com/watch?v=Wm15rvkifPc | castnow --device "$CHROMECAST_DEV" --quiet -
+        killall_castnow_processes
+        play_youtube https://www.youtube.com/watch?v=Wm15rvkifPc
     done
-done" &
+done &
 
-sudo bash -c "while :; do
+while :; do
     tail -f -n0 $LOG_FILE | stdbuf -o0 bgrep -b \\\"\<KP8\>\\\" | while read -r; do
         echo ===== Playing Animal Lullaby =====
-        pkill node
-        youtube-dl -o - https://www.youtube.com/watch?v=IofXhvcafuo | castnow --device "$CHROMECAST_DEV" --quiet -
+        killall_castnow_processes
+        play_youtube https://www.youtube.com/watch?v=IofXhvcafuo
     done
-done" &
+done &
 
-sudo bash -c "while :; do
+while :; do
     tail -f -n0 $LOG_FILE | stdbuf -o0 bgrep -b \\\"\<KP9\>\\\" | while read -r; do
         echo ===== Playing White Noise =====
-        pkill node
-        youtube-dl -o - https://www.youtube.com/watch?v=j8L5vrTHhHs | castnow --device "$CHROMECAST_DEV" --quiet -
+        killall_castnow_processes
+        play_youtube https://www.youtube.com/watch?v=j8L5vrTHhHs
     done
-done" &
+done &
 
 #
 # symbol mappings
 #
 
-sudo bash -c "while :; do
+while :; do
     tail -f -n0 $LOG_FILE | stdbuf -o0 bgrep -b \\\"\<KP/\>\\\" | while read -r; do
         echo ===== Playing OSU Marching Band =====
-        pkill node
-        youtube-dl --playlist-random -o - https://www.youtube.com/playlist?list=PLyXb8EZU88Q-qSxRiLOzTcn1Rer7VMgJa | castnow --device "$CHROMECAST_DEV" --quiet -
+        killall_castnow_processes
+        play_youtube_playlist_random https://www.youtube.com/playlist?list=PLyXb8EZU88Q-qSxRiLOzTcn1Rer7VMgJa
     done
-done" &
+done &
 
-sudo bash -c "while :; do
+while :; do
     tail -f -n0 $LOG_FILE | stdbuf -o0 bgrep -b \\\"\<KP*\>\\\" | while read -r; do
         echo ===== Playing Baby Crying =====
-        pkill node
-        youtube-dl -o - https://www.youtube.com/watch?v=qS7nqwGt4-I | castnow --device "$CHROMECAST_DEV" --quiet -
+        killall_castnow_processes
+        play_youtube https://www.youtube.com/watch?v=qS7nqwGt4-I
     done
-done"
+done
 # last command here intentionally missing '&'
