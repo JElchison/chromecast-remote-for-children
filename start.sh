98037a79d8c5997a10ad05aee4173fbd9db3cf7d
Jonathan Elchison <JElchison@Gmail.com>
2017-12-19 08:25:18 -0500

    Narrow castnow kill logic

--- start.sh
+++ start.sh
@@ -24,7 +24,7 @@ trap "trap - SIGTERM && kill -- -$$" SIGINT SIGTERM EXIT
 sudo bash -c "while :; do
     tail -f -n0 $LOG_FILE | stdbuf -o0 bgrep -b \\\"\<Esc\>\\\" | while read -r; do
         echo ===== Stop =====
-        pkill -f 'node.*castnow'
+        pkill node
         castnow --device $CHROMECAST_DEV --command s --exit
     done
 done" &
@@ -57,7 +57,7 @@ done" &
 sudo bash -c "while :; do
     tail -f -n0 $LOG_FILE | stdbuf -o0 bgrep -b \\\"\<KP0\>\\\" | while read -r; do
         echo ===== Playing Polaroid Memories =====
-        pkill -f 'node.*castnow'
+        pkill node
         youtube-dl -o - https://www.youtube.com/watch?v=shNP5H13a1M | castnow --device "$CHROMECAST_DEV" --quiet -
     done
 done" &
@@ -65,7 +65,7 @@ done" &
 sudo bash -c "while :; do
     tail -f -n0 $LOG_FILE | stdbuf -o0 bgrep -b \\\"\<KP1\>\\\" | while read -r; do
         echo ===== Playing Claymation Christmas =====
-        pkill -f 'node.*castnow'
+        pkill node
         youtube-dl -o - https://www.youtube.com/playlist?list=PLd6uqVNEu-peflL0eT4lOwVrRnshaod98 | castnow --device "$CHROMECAST_DEV" --quiet -
     done
 done" &
@@ -73,7 +73,7 @@ done" &
 sudo bash -c "while :; do
     tail -f -n0 $LOG_FILE | stdbuf -o0 bgrep -b \\\"\<KP2\>\\\" | while read -r; do
         echo ===== Playing The Snowman =====
-        pkill -f 'node.*castnow'
+        pkill node
         youtube-dl -o - https://www.youtube.com/watch?v=ZE9KpobX9J8 | castnow --device "$CHROMECAST_DEV" --quiet -
     done
 done" &
@@ -81,7 +81,7 @@ done" &
 sudo bash -c "while :; do
     tail -f -n0 $LOG_FILE | stdbuf -o0 bgrep -b \\\"\<KP3\>\\\" | while read -r; do
         echo ===== Playing Carl the Super Truck =====
-        pkill -f 'node.*castnow'
+        pkill node
         youtube-dl -o - https://www.youtube.com/watch?v=JUeru7ioMyk | castnow --device "$CHROMECAST_DEV" --quiet -
     done
 done" &
@@ -89,7 +89,7 @@ done" &
 sudo bash -c "while :; do
     tail -f -n0 $LOG_FILE | stdbuf -o0 bgrep -b \\\"\<KP4\>\\\" | while read -r; do
         echo ===== Playing Mister Rogers =====
-        pkill -f 'node.*castnow'
+        pkill node
         youtube-dl --playlist-random -o - https://www.youtube.com/playlist?list=PLf22vOV7unX-ly5LC98UyZTOSG8ckDqmt | castnow --device "$CHROMECAST_DEV" --quiet -
     done
 done" &
@@ -97,7 +97,7 @@ done" &
 sudo bash -c "while :; do
     tail -f -n0 $LOG_FILE | stdbuf -o0 bgrep -b \\\"\<KP5\>\\\" | while read -r; do
         echo ===== Playing Daniel Tiger =====
-        pkill -f 'node.*castnow'
+        pkill node
         youtube-dl --playlist-random -o - https://www.youtube.com/playlist?list=PLKzJHcOwWn3KWJco0w6gVvhcrhXDb4TFi | castnow --device "$CHROMECAST_DEV" --quiet -
     done
 done" &
@@ -105,7 +105,7 @@ done" &
 sudo bash -c "while :; do
     tail -f -n0 $LOG_FILE | stdbuf -o0 bgrep -b \\\"\<KP6\>\\\" | while read -r; do
         echo ===== Playing Thomas and Friends =====
-        pkill -f 'node.*castnow'
+        pkill node
         youtube-dl --playlist-random -o - https://www.youtube.com/playlist?list=PLI2i4PrLia3gAAYjMaVbAwdt1xrOsUaXS | castnow --device "$CHROMECAST_DEV" --quiet -
     done
 done" &
@@ -113,7 +113,7 @@ done" &
 sudo bash -c "while :; do
     tail -f -n0 $LOG_FILE | stdbuf -o0 bgrep -b \\\"\<KP7\>\\\" | while read -r; do
         echo ===== Playing Colored Lullaby =====
-        pkill -f 'node.*castnow'
+        pkill node
         youtube-dl -o - https://www.youtube.com/watch?v=Wm15rvkifPc | castnow --device "$CHROMECAST_DEV" --quiet -
     done
 done" &
@@ -121,7 +121,7 @@ done" &
 sudo bash -c "while :; do
     tail -f -n0 $LOG_FILE | stdbuf -o0 bgrep -b \\\"\<KP8\>\\\" | while read -r; do
         echo ===== Playing Animal Lullaby =====
-        pkill -f 'node.*castnow'
+        pkill node
         youtube-dl -o - https://www.youtube.com/watch?v=IofXhvcafuo | castnow --device "$CHROMECAST_DEV" --quiet -
     done
 done" &
@@ -129,7 +129,7 @@ done" &
 sudo bash -c "while :; do
     tail -f -n0 $LOG_FILE | stdbuf -o0 bgrep -b \\\"\<KP9\>\\\" | while read -r; do
         echo ===== Playing White Noise =====
-        pkill -f 'node.*castnow'
+        pkill node
         youtube-dl -o - https://www.youtube.com/watch?v=j8L5vrTHhHs | castnow --device "$CHROMECAST_DEV" --quiet -
     done
 done" &
@@ -141,7 +141,7 @@ done" &
 sudo bash -c "while :; do
     tail -f -n0 $LOG_FILE | stdbuf -o0 bgrep -b \\\"\<KP/\>\\\" | while read -r; do
         echo ===== Playing OSU Marching Band =====
-        pkill -f 'node.*castnow'
+        pkill node
         youtube-dl --playlist-random -o - https://www.youtube.com/playlist?list=PLyXb8EZU88Q-qSxRiLOzTcn1Rer7VMgJa | castnow --device "$CHROMECAST_DEV" --quiet -
     done
 done" &
@@ -149,7 +149,7 @@ done" &
 sudo bash -c "while :; do
     tail -f -n0 $LOG_FILE | stdbuf -o0 bgrep -b \\\"\<KP*\>\\\" | while read -r; do
         echo ===== Playing Baby Crying =====
-        pkill -f 'node.*castnow'
+        pkill node
         youtube-dl -o - https://www.youtube.com/watch?v=qS7nqwGt4-I | castnow --device "$CHROMECAST_DEV" --quiet -
     done
 done"
